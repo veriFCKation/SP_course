8b219682f48fe4cf8772ff427eb538613af0b26e
veriFCKation <99489584+veriFCKation@users.noreply.github.com>
2023-06-20 21:18:25 +0300

    init

---
+++ HW1/code.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#incldue<stdlib.h>
+
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+ 
+// Partition the array using the last element as the pivot
+int partition(int arr[], int low, int high)
+{
+    // Choosing the pivot
+    int pivot = arr[high];
+ 
+    // Index of smaller element and indicates
+    // the right position of pivot found so far
+    int i = (low - 1);
+ 
+    for (int j = low; j <= high - 1; j++) {
+ 
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot) {
+ 
+            // Increment index of smaller element
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
